7323a15aa0879dc51bf27138e3000a1c57b1932f
Felipe Godinho Dal Molin <felipegodinhodalmolin@gmail.com>
2024-11-15 17:35:44 -0300

    DFS

--- red_black_tree/main.c
+++ red_black_tree/main.c
@@ -6,9 +6,18 @@ void visitar(int valor){
     printf("%d ", valor);
 }
 
+void dfs(No* raiz, No* nulo) {
+	if(raiz != nulo) {
+		printf("%d ", raiz->valor);
+		dfs(raiz->esquerda, nulo);
+		dfs(raiz->direita, nulo);
+	}
+}
+
 int main() {
     Arvore* a = criar();
-
+    No* nulo = a->nulo;
+    
     adicionar(a,7);
     adicionar(a,6);
     adicionar(a,5);
@@ -17,7 +26,9 @@ int main() {
     adicionar(a,2);
     adicionar(a,1);
 
-    printf("In-order: ");
-    percorrerProfundidadeInOrder(a, a->raiz,visitar);
-    printf("\n");
+    dfs(a->raiz, nulo);
+
+    // printf("In-order: ");
+    // percorrerProfundidadeInOrder(a, a->raiz,visitar);
+    // printf("\n");
 }
