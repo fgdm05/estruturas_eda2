0ac0c0d040d80d554beec5dc35459d64dd9774b0
Felipe Godinho Dal Molin <felipegodinhodalmolin@gmail.com>
2024-11-25 20:34:46 -0300

    Refatoração 1, removendo contagem do tempo

--- main.c
+++ main.c
@@ -2,51 +2,26 @@
 #include "red_black_tree/def_test_cases.h"
 #include <math.h>
 
-void arredondar(double* a, int casas) {
-    double pot = (double) (pow(10.0,casas));
-    *a = round(pot*(*a))/pot;
-}
-
 // O caso sera calculado i_precisao vezes para obter uma media
 void calcularCaso(int n, int i_precisao, int casas) {
     FILE* avl_ins = fopen("avl_ins.csv","a");
     FILE* avl_rem = fopen("avl_rem.csv","a");
     FILE* rb_ins = fopen("rb_ins.csv","a");
     FILE* rb_rem = fopen("rb_rem.csv","a");
-    double tm_avl_insert = 0, tm_avl_remove = 0, 
-    tm_rb_insert = 0, tm_rb_remove = 0;
     int ops_ins_avl_m = 0;
     int ops_rem_avl_m = 0;
     int ops_ins_rb_m = 0;
     int ops_rem_rb_m = 0;
     for(int i = 0; i < i_precisao; i++) {
-        double t_avl_insert, t_avl_remove, 
-            t_rb_insert, t_rb_remove;
         int ops_ins_avl = 0, ops_rem_avl = 0; 
         int ops_ins_rb = 0, ops_rem_rb = 0;       
-        heavyTest(n, &t_avl_insert, &t_avl_remove, &ops_ins_avl, &ops_rem_avl);
-        heavyTest_rb(n, &t_rb_insert, &t_rb_remove, &ops_ins_rb, &ops_rem_rb);
-        arredondar(&t_avl_insert,casas);
-        arredondar(&t_avl_remove,casas);
-        arredondar(&t_rb_insert,casas);
-        arredondar(&t_rb_remove,casas);
-        tm_avl_insert += t_avl_insert;
-        tm_avl_remove += t_avl_remove;
-        tm_rb_insert += t_rb_insert;
-        tm_rb_remove += t_rb_remove;
+        heavyTest(n, &ops_ins_avl, &ops_rem_avl);
+        heavyTest_rb(n, &ops_ins_rb, &ops_rem_rb);
         ops_ins_avl_m += ops_ins_avl;
         ops_rem_avl_m += ops_rem_avl;
         ops_ins_rb_m += ops_ins_rb;
         ops_rem_rb_m += ops_rem_rb;
     }
-    tm_avl_insert /= (double)i_precisao;
-    tm_avl_remove /= (double)i_precisao;
-    tm_rb_insert /= (double)i_precisao;
-    tm_rb_remove /= (double)i_precisao;
-    arredondar(&tm_avl_insert,casas);
-    arredondar(&tm_avl_remove,casas);
-    arredondar(&tm_rb_insert,casas);
-    arredondar(&tm_rb_remove,casas);
     ops_ins_avl_m /= i_precisao;
     ops_rem_avl_m /= i_precisao;
     ops_ins_rb_m /= i_precisao;
@@ -55,10 +30,10 @@ void calcularCaso(int n, int i_precisao, int casas) {
     fprintf(avl_rem, "%d;%d\n",n,ops_rem_avl_m);
     fprintf(rb_ins, "%d;%d\n",n,ops_ins_rb_m);
     fprintf(rb_rem, "%d;%d\n",n,ops_rem_rb_m);
-    printf("\nAVL insert (tm): %lf (ms) - %d", tm_avl_insert, ops_ins_avl_m);
-    printf("\nAVL remove (tm): %lf (ms) - %d", tm_avl_remove, ops_rem_avl_m);
-    printf("\nRB insert (tm): %lf (ms) - %d", tm_rb_insert, ops_ins_rb_m);
-    printf("\nRB remove (tm): %lf (ms) - %d", tm_rb_remove, ops_rem_rb_m);
+    printf("\nAVL insert %d", ops_ins_avl_m);
+    printf("\nAVL remove %d", ops_rem_avl_m);
+    printf("\nRB insert %d", ops_ins_rb_m);
+    printf("\nRB remove %d", ops_rem_rb_m);
     fclose(avl_ins);
     fclose(avl_rem);
     fclose(rb_ins);
--- red_black_tree/def_test_cases.h
+++ red_black_tree/def_test_cases.h
@@ -1,4 +1,4 @@
 #ifndef DEF_TEST_CASES_RB
 #define DEF_TEST_CASES_RB
-void heavyTest_rb(int num_nos, double*, double*, int*, int*);
+void heavyTest_rb(int num_nos, int*, int*);
 #endif
